cdf7acff82eee9fadaeaffa9fd043cce2d16f5b0
Min Thu Zaw <minthuzaw775@gmail.com>
2023-02-06 19:04:21 +0630

    learning skeleton loading with css

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("data.json")
+  .then((response) => response.json())
+  .then((posts) => {
+    container.innerHTML = "";
+    posts.forEach((post) => {
+      const div = cardTemplate.content.cloneNode(true);
+      div.getElementById("card-link").href = post.link;
+      div.getElementById("logo-img").src = post.logoImage;
+      div.getElementById("card-title").textContent = post.title;
+      div.getElementById("card-details").textContent = post.details;
+      div.getElementById("cover-img").src = post.coverImage;
+      div.getElementById(
+        "card-footer"
+      ).innerHTML = ` <ion-icon name="arrow-up"></ion-icon>
+          <ion-icon name="chatbox-ellipses"></ion-icon>
+          <ion-icon name="bookmark"></ion-icon>`;
+      container.append(div);
+    });
+  });
